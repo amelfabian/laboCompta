948dbf7fd75b1127d0dc363f346d0170243d97ab
amelfabian <amelfabian@gmail.com>
2017-06-15 09:27:44 +0200

    Factorisation via newInput

--- laboCompta_AF/src/be/steformations/model/CompteGeneral.java
+++ laboCompta_AF/src/be/steformations/model/CompteGeneral.java
@@ -4,58 +4,76 @@ import java.util.Scanner;
 import java.util.TreeMap;
 
 public class CompteGeneral {
-	
+
 	static Scanner sc = new Scanner(System.in);
+	static String rep;
+
 	public CompteGeneral() {
 	}
 
 	public void setValid(boolean b) {
-		
+
 	}
 
 	public boolean isValid() {
-		
+
 		return true;
 	}
 
 	public void creer() {
-		
-		System.out.println("le compte doit-il être subdiviser (oui/non)?: ");
-		String rep = sc.nextLine();
-		if(rep.equals("oui")){
-			entrerNum();
-		}else{
-			if(rep.equals("non")){
-			 entrerNum();	 
-			}
-			else{
-				System.out.println("try again");
-			}
-		}
-		System.out.println("");
-	}
-	
 
-	public void entrerNum(){
+		System.out.println("le compte doit-il être subdiviser (oui/non)?: ");
+		rep = sc.nextLine();
+		do {
+
+			if (rep.equals("oui")) {
+				newInput();
+				break;
+			} else {
+				if (rep.equals("non")) {
+					newInput();
+					break;
+				} else {
+					System.out.println("try again");
+					System.out.println("le compte doit-il être subdiviser (oui/non)?: ");
+					rep = sc.nextLine();
+				}
+			}
+
+		} while (!(rep.equals("oui")) && (!(rep.equals("oui"))));
+
+	}
+
+	public int sizeNum(String string) {
+		int cpt = 0;
+		while (string.substring(cpt).equalsIgnoreCase(null)) {
+			string.substring(cpt);
+		}
+		return cpt;
+
+	}
+
+	public void newInput() {
 		System.out.println("Entrez un numéro de compte");
-		String num = sc.nextLine();
+		rep = sc.nextLine();
+		if (sizeNum(rep) > 1 || sizeNum(rep) < 6) {
+			System.out.println("entrez le nom");
+		}
 	}
 
 	public void modifier() {
 		System.out.println("CompteGeneral.modifier()");
-		
+
 	}
 
 	public void supprimer() {
-	System.out.println("CompteGeneral.supprimer()");
-		
+		System.out.println("CompteGeneral.supprimer()");
+
 	}
 
 	public void lister() {
-	System.out.println("CompteGeneral.lister()");
-		
+		System.out.println("CompteGeneral.lister()");
+
 	}
-
-
 
 }
---
+++ laboCompta_AF/test/testControle/testControleCompte.java
@@ -0,0 +1,14 @@
+package testControle;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testControleCompte {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
