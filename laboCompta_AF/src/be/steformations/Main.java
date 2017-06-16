a4073679df10299ffcfa6569596369b64de84f82
yannick <yannick.boogaerts@ulg.ac.be>
2017-06-16 12:11:35 +0200

    modification des méthodes static

--- laboCompta_AF/src/be/steformations/Main.java
+++ laboCompta_AF/src/be/steformations/Main.java
@@ -11,7 +11,8 @@ public class Main {
 
 
 	public static void main(String[] args) {
-		Builder.start();
+		Builder b = new Builder();
+		b.start();
 		
 	
 
--- laboCompta_AF/src/be/steformations/controle/Builder.java
+++ laboCompta_AF/src/be/steformations/controle/Builder.java
@@ -8,42 +8,39 @@ import be.steformations.vue.VueCompte;
 
 public class Builder {
 
-	static Scanner scan = new Scanner(System.in);
-	
-	private static int choice;
-	
-	public static void start() {
+	public void start() {
+		int choice;
+		Scanner scan = new Scanner(System.in);
 		System.out.println(VueCompte.getScreen());
-		NewInput();
-		switch(choice){
-		case 1: CompteGeneral.creer(); 
-		break;
-		case 2: CompteGeneral.modifier();
-		break;
-		case 3: CompteGeneral.supprimer();
-		break; 
-		case 4: CompteGeneral.lister();
-		break;
-		case 5: CompteParticulier.creer();
-		break;
-		case 6: CompteParticulier.modifier();
-		break;
-		case 7: CompteParticulier.supprimer();
-		break;
-		case 8: CompteParticulier.lister();
-		break;
+		choice = scan.nextInt();
+		switch (choice) {
+		case 1:
+			CompteGeneral.creer();
+			break;
+		case 2:
+			CompteGeneral.modifier();
+			break;
+		case 3:
+			CompteGeneral.supprimer();
+			break;
+		case 4:
+			CompteGeneral.lister();
+			break;
+		case 5:
+			CompteParticulier.creer();
+			break;
+		case 6:
+			CompteParticulier.modifier();
+			break;
+		case 7:
+			CompteParticulier.supprimer();
+			break;
+		case 8:
+			CompteParticulier.lister();
+			break;
 		default:
 		}
-	
+
 	}
-	
-	
-	
-	
-	
-	public static void NewInput()
-	{
-	choice = scan.nextInt();
-	}
-	
+
 }
