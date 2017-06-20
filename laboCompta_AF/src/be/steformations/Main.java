fcbbe4b31195637ea48e76a593e40dfd2affd146
DaN <danesh.rostaei@gmail.com>
2017-06-20 15:06:03 +0200

    Voulez-vous continuer ?

--- laboCompta_AF/src/be/steformations/Main.java
+++ laboCompta_AF/src/be/steformations/Main.java
@@ -1,5 +1,7 @@
 package be.steformations;
 
+import java.util.Scanner;
+
 import be.steformations.controle.Builder;
 
 
@@ -7,9 +9,19 @@ public class Main {
 
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		Builder b = new Builder();
-		b.Start();
+		
+		System.out.println("Commencé (O/N) ? :");
+		Character sub = sc.nextLine().charAt(0);
+
+		while (Character.toUpperCase(sub) == 'O')  {
+			b.Start();
+			System.out.println("Continuer (O/N) ? :");
+			sub = sc.nextLine().charAt(0);
+
 		}
+		System.out.println("Dazvidaniya");
 	
 
 	}   
